8f9e97a6c09bdbb6fdf6144ef800c47c59966990
Leandro Moraes <lbc_moraes@outlook.com>
2026-06-03 22:38:11 -0300

    feat(persistence): add ChargeJpaEntity mapped to charges table

---
+++ src/main/java/com/lmoraesdev/payment/adapter/out/persistence/ChargeJpaEntity.java
@@ -0,0 +1,61 @@
+package com.lmoraesdev.payment.adapter.out.persistence;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import com.lmoraesdev.payment.domain.model.ChargeStatus;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "charges")
+public class ChargeJpaEntity {
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ChargeStatus status;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+    protected ChargeJpaEntity(){}
+
+    public UUID getId(){
+        return id;
+    }
+
+    public void setId(UUID id){
+        this.id = id;
+    }
+
+    public BigDecimal getAmount(){
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount){
+        this.amount = amount;
+    }
+
+    public ChargeStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ChargeStatus status){
+        this.status = status;
+    }
+
+    public Instant getCreatedAt(){
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt){
+        this.createdAt = createdAt;
+    }
+
+}
