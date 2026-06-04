3542c78345acf505b99e0e79015168fb23bb1776
Leandro Moraes <lbc_moraes@outlook.com>
2026-06-03 22:38:15 -0300

    feat(persistence): add Spring Data JPA repository

---
+++ src/main/java/com/lmoraesdev/payment/adapter/out/persistence/SpringDataChargeRepository.java
@@ -0,0 +1,9 @@
+package com.lmoraesdev.payment.adapter.out.persistence;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpringDataChargeRepository
+    extends JpaRepository<ChargeJpaEntity, UUID>{
+}
