506a603847389f9b075d801fa49ea2d6d92991dd
Leandro Moraes <lbc_moraes@outlook.com>
2026-06-05 16:06:51 -0300

    feat(application): add business-event logging to CreateChargeService
    Log charge_created in INFO after successful save, capturing the
    business event without leaking infrastructure details.

--- src/main/java/com/lmoraesdev/payment/application/usecase/CreateChargeService.java
+++ src/main/java/com/lmoraesdev/payment/application/usecase/CreateChargeService.java
@@ -4,6 +4,7 @@ import com.lmoraesdev.payment.application.port.in.CreateCharge;
 import com.lmoraesdev.payment.application.port.in.CreateChargeCommand;
 import com.lmoraesdev.payment.application.port.in.CreateChargeResult;
 import com.lmoraesdev.payment.application.port.out.ChargeRepository;
+import com.lmoraesdev.payment.config.logging.Logger5w1hBuilder;
 import com.lmoraesdev.payment.domain.model.Charge;
 import com.lmoraesdev.payment.domain.model.Money;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,14 @@ public class CreateChargeService implements CreateCharge {
         Charge charge = Charge.create(amount);
 
         Charge saved = chargeRepository.save(charge);
+
+        Logger5w1hBuilder.create(CreateChargeService.class)
+                .where("CreateChargeService")
+                .what("charge_created")
+                .why("charge creation requested")
+                .who("system")
+                .how("createCharge")
+                .info();
 
         return new CreateChargeResult(
                 saved.getId(),
